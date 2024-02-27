fa54d970b66355ddda59c09688c23f99a619c0d3
Shruti Dongre <155142224+Shruti11D@users.noreply.github.com>
2024-02-27 08:01:48 +0000

    dataTypes.pract

---
+++ 01-basics/02-dataTypes.js
@@ -0,0 +1,27 @@
+"use strict"; // treat all JS code as newer version
+
+//alert ( 3 + 3) //we are using nodejs, not browser
+
+// console.log(3+3)
+
+//code readability should be high
+
+let name = "shruti" // "" is string
+let age = 21 // number
+let isLoggedIn = false // boolean
+let state; // undefined
+
+
+// number = 2 ro power 53
+// bigint
+// string = ""
+// boolean = true/false
+// null = standalone value
+// undefined = value not defined yet
+// symbol = unique
+
+//object
+
+console.log(typeof "shruti"); //string
+console.log(typeof null); //object
+console.log(typeof undefined); //undefined
