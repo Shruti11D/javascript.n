e44c2a27e14d077951eb400f298bca95650ab79e
Shruti Dongre <155142224+Shruti11D@users.noreply.github.com>
2024-02-27 06:36:16 +0000

    variables.practice

---
+++ 01-basics/01-variables.js
@@ -0,0 +1,22 @@
+const accountId = 1234321
+let accountEmail = "shrutid12@gmail.com"
+var accountPassword = "12341"
+accountCity = "jaipur"
+// accountState;
+
+// accountId = 2 not allowed
+
+// console.log(accountId);
+
+
+accountId = 1234321
+accountEmail = "sd1@gmail.com"
+accountPassword = "0101"
+accountCity = "punjab"
+let accountState;
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
+
+
+// prefer not to use var
+// because of issue in block space and functional scope
